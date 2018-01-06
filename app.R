92edf38af9a7f2be3ab7ad631fda53f0e654cbf5
pvaiciunas <p.l.vaiciunas@gmail.com>
2018-01-06 15:35:53 -0500

    Working app Issues with graph NEed decimilzation for input need better design

--- app.R
+++ app.R
@@ -1,60 +1,98 @@
 library(shiny)
 library(rhandsontable)
+library(tidyr)
+library(ggplot2)
 
 source("helpers.R")
-
+source("helpers2.R")
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   rHandsontableOutput('table'),
   textOutput('result'),
-  plotOutput('plot')
+  tableOutput('covmat'),
+  plotOutput('plot'),
+  tableOutput('rbind_table'),
+  
   radioButtons('radio_buttons', 
                label = "CMA or Historical Volatility?",
-               choices = c("CMAs" = "cma",
-                           "Historical" = "historical"))
+               choices = c("CMA!!!" = "cma",
+                           "Historical" = "historical")),
+  numericInput('cash_rate', label = "Cash Rate:", value = 0)
+  
 )
 
 
 # Define server logic required to draw a histogram
 server <- function(input,output,session)({
 
-  values <- reactiveValues(data=wgt_deltas) # datatable found in helpers2  
-  
-    
-  weightInput <- reactive({
-  
-  })
-  
-  # Choose the right covariance matrix
-  portfolio_vols <- reactive({
-    cov_mat <- ifelse(input$radio_buttons == "CMAs", cma_covmat, hist_covmat)
-  })
-  
-  
-  ####
-  # Do you choose the 
-  
+  # The table up top
+  values <- reactiveValues(data = wgt_delta) # datatable found in helpers2  
   
   observe({
     if(!is.null(input$table))
       values$data <- hot_to_r(input$table)
   })
   
-  
   output$table <- renderRHandsontable({
     rhandsontable(values$data)
   })
   
-  
-  output$result <- renderText({ 
-    sum(values$data)
+  # Choose the right covariance matrix
+  covmat_final <- reactive({
+    if(input$radio_buttons == "cma") {
+      covmat_cma 
+    } else if (input$radio_buttons == "historical") {
+      covmat_hist 
+    }
   })
   
-  output$plot <- renderPlot({
-    new_wgts <- starting_wgts + wgt_deltas
+  # CAsh Rate
+  cash_rate <- reactive({
+    as.numeric(input$cash_rate)
+  })
+  
+  # Port Returns
+  port_return <- reactive({ 
+    as.double(colSums(mapply("*",(values$data + wgt_base),returns_cma)))
+  })
+  
+  # Port Vol
+  port_vol <- reactive({
+    calcPortfolioVols2(covmat_final(), (values$data + wgt_base))
+  })
+  
+  # Port Sharpe
+  port_sharpe <- reactive({
+    as.double((port_return() - cash_rate()) / port_vol())
+  })
+  
+ 
+  output$covmat <- renderTable ({
+    covmat_final()
+  })
+  
+  output$rbind_table <- renderTable ({
+    p_data <- rbind(as.double(port_return()), as.double(port_sharpe()), as.double(port_vol())) %>%
+      as.data.frame() %>%
+      mutate(measure = c("return", "sharpe", "volatility")) %>%
+      gather(key = measure)
     
-    ggplot()
-      
+    colnames(p_data) <- c("measure","portfolio","value")
+    
+    p_data
+  })
+
+  output$plot <- renderPlot({
+    p_data <- rbind(as.double(port_return()), as.double(port_sharpe()), as.double(port_vol())) %>%
+    as.data.frame() %>%
+    mutate(measure = c("return", "sharpe", "volatility")) %>%
+    gather(key = measure)
+
+    colnames(p_data) <- c("measure","portfolio","value")
+  
+    ggplot(p_data, aes(x = portfolio, y = value)) +
+      geom_line() +
+      facet_wrap(~measure)
   })
 }) 
 
--- helpers.R
+++ helpers.R
@@ -51,8 +51,8 @@ calcPortfolioVols2 <- function(covMat,weights) {
   #
   # Inputs
   #
-  # covMat:   Cov Matrix. class MAtrix Should be labeled for integrity check
-  # weights:  weight dataframe. Asset classes in column 1. Portfolio names 
+  # covMat:   Cov Matrix. class MAtrix. Should be labeled for integrity check
+  # weights:  weight dataframe. Asset classes are rownames. Portfolio names 
   #           in columns. Weights underneath the portfolio names
   
   
@@ -63,55 +63,52 @@ calcPortfolioVols2 <- function(covMat,weights) {
   asset_names <- colnames(covMat)
 
   # Before you make calcs, make sure the weights are in the same
-  # order as returns. 
-  weights$asset <- factor(weights$asset, levels = asset_names)
-  weights <- arrange(weights, asset)
-  m_weights <- as.matrix(select(weights, -asset))
+  # order as the covariance matrix, and turn into a matrix
+ # weights <- weights[order(rownames(covMat)),]
+  m_weights <- as.matrix(weights)
   
-    # Calc the overall risk stats
-  port_vol <- sqrt(diag(t(m_weights) %*% covMat %*% m_weights))
+  # Calc portfolio annualized vols
+  port_vol <- sqrt(diag(t(m_weights) %*% covMat %*% m_weights))*sqrt(252)
   
-  # Return annualized port_vol
-  port_vol * sqrt(252)
-  
+  port_vol
 }
 
-set.seed(1234)
-# Set up sample data. wIll fill this in later
-data <- data.frame(date = rnorm(20), 
-                   stock1 = rnorm(20)/100, 
-                   stock2 = rnorm(20)/100,
-                   stock3 = rnorm(20)/100)
-# PORTFOLIOS ARE COLUMNS
-weights <- data.frame(asset = c("stock2", "stock1", "stock3"), 
-                      port1 = c(0.40, 0.3, 0.3),
-                      port2 = c(0.5, 0.2, 0.3),
-                      port3 = c(0.1,0.1,0.8),
-                      port4 = c(0.2,0.5,0.3))
-
-# Store the max and min dates for reference, as well as the stock names
-max_date <- max(data$date)
-min_date <- min(data$date)
-asset_names <- names(data)[-1]
-
-# get the returns into matrix form
-m_returns <- data %>%
-  select(-date) %>%
-  as.matrix
-
-
-# Before you make calcs, make sure the weights are in the same
-# order as returns. 
-weights$asset <- factor(weights$asset, levels = asset_names)
-weights <- arrange(weights, asset)
-m_weights <- as.matrix(select(weights, -asset))
-
-# Calc the covariance matrix
-covariance <- cov(m_returns)
-
-# Calc the overall risk stats
-port_vol <- sqrt(diag(t(m_weights) %*% covariance %*% m_weights))# The diag is the vol?
-# Need to use 'sweep here'. Simply divinding by port_vols makes the operation go row-wise first
-marginal_vols <- sweep(covariance %*% m_weights, MARGIN = 2, port_vol, '/') 
-vol_contribution <- marginal_vols * m_weights
-
+# set.seed(1234)
+# # Set up sample data. wIll fill this in later
+# data <- data.frame(date = rnorm(20), 
+#                    stock1 = rnorm(20)/100, 
+#                    stock2 = rnorm(20)/100,
+#                    stock3 = rnorm(20)/100)
+# # PORTFOLIOS ARE COLUMNS
+# weights <- data.frame(asset = c("stock2", "stock1", "stock3"), 
+#                       port1 = c(0.40, 0.3, 0.3),
+#                       port2 = c(0.5, 0.2, 0.3),
+#                       port3 = c(0.1,0.1,0.8),
+#                       port4 = c(0.2,0.5,0.3))
+# 
+# # Store the max and min dates for reference, as well as the stock names
+# max_date <- max(data$date)
+# min_date <- min(data$date)
+# asset_names <- names(data)[-1]
+# 
+# # get the returns into matrix form
+# m_returns <- data %>%
+#   select(-date) %>%
+#   as.matrix
+# 
+# 
+# # Before you make calcs, make sure the weights are in the same
+# # order as returns. 
+# weights$asset <- factor(weights$asset, levels = asset_names)
+# weights <- arrange(weights, asset)
+# m_weights <- as.matrix(select(weights, -asset))
+# 
+# # Calc the covariance matrix
+# covariance <- cov(m_returns)
+# 
+# # Calc the overall risk stats
+# port_vol <- sqrt(diag(t(m_weights) %*% covariance %*% m_weights))# The diag is the vol?
+# # Need to use 'sweep here'. Simply divinding by port_vols makes the operation go row-wise first
+# marginal_vols <- sweep(covariance %*% m_weights, MARGIN = 2, port_vol, '/') 
+# vol_contribution <- marginal_vols * m_weights
+# 
